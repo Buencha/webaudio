bb8ee902b8867a00932174e804b3553bdd9c7386
BNC <BNC>
2017-04-03 23:34:52 -0300

    additive synth adding type and gain

--- module.js
+++ module.js
@@ -2,3 +2,23 @@
 	var audio_context = window.AudioContext || window.webKitAudioContext ;
 
 	var con = new audio_context();
+	
+
+	$(".background").keydown(function (){
+	var osc = con.createOscillator();
+
+	var amp = con.createGain();
+
+	amp.gain.value = 0.05;
+
+	osc.frequency.value = Math.random() * 225;
+	osc.type = "triangle";
+	
+
+	osc.connect(amp);
+	amp.connect(con.destination);
+	
+	//osc.start();
+
+	});
+  
