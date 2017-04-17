bfd66335082d52c407d24139ad73d9692b4209ae
Emmanuel Bravo <buenncha@gmail.com>
2017-04-17 00:12:24 -0300

    nexus

--- module.js
+++ module.js
@@ -2,24 +2,24 @@
 	var audio_context = window.AudioContext || window.webKitAudioContext ;
 
 	var con = new audio_context();
-	var filter = con.createBiquadFilter();
+	// var filter = con.createBiquadFilter();
 
 
 	var osc = con.createOscillator();
 	osc.frequency.value = 440;
 
 	osc.type = "sawtooth"
-	osc.connect(filter);
-	osc.start();
-	filter.connect(con.destination);
+	osc.connect(con.destination);
+	// osc.start();
+	// filter.connect(con.destination);
 	
-	$(".background").mousemove(function(event) {
-		filter.frequency.value = event.clientX;
-		filter.Q.value = event.clientY / 5;
-	});
-		$(".background").keydown(function (){
+	// $(".background").mousemove(function(event) {
+	// 	filter.frequency.value = event.clientX;
+	// 	filter.Q.value = event.clientY / 5;
+	// });
+	// 	$(".background").keydown(function (){
 
-	osc.frequency.value = osc.frequency.value + 10;
+	// osc.frequency.value = osc.frequency.value + 10;
 
-	});
+	// });
   
