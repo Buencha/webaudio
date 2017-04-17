60653ee8e1a8a000a889744db558797d20abf4d6
BNC <BNC>
2017-04-16 22:17:43 -0300

    filter on mouse X

--- module.js
+++ module.js
@@ -2,28 +2,23 @@
 	var audio_context = window.AudioContext || window.webKitAudioContext ;
 
 	var con = new audio_context();
-	
+	var filter = con.createBiquadFilter();
 
 
-
-	$(".background").keydown(function (){
 	var osc = con.createOscillator();
+	osc.frequency.value = 440;
 
-	var amp = con.createGain();
-
-	osc.frequency.value = Math.random() * 225;
-	osc.connect(amp);
-
-
-	now = con.currentTime;	
-	
-	amp.gain.value = 0;
-	amp.gain.linearRampToValueAtTime(0.1, now + 2);	
-	amp.gain.linearRampToValueAtTime(0, now + 4);	
-
-	amp.connect(con.destination);
+	osc.type = "sawtooth"
+	osc.connect(filter);
 	osc.start();
-	osc.start(now + 4);
+	filter.connect(con.destination);
+	
+	$(".background").mousemove(function(event) {
+		filter.frequency.value = event.clientX;
+	});
+		$(".background").keydown(function (){
+
+	osc.frequency.value = osc.frequency.value + 10;
 
 	});
   
